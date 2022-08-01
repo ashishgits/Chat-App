a99cb78f3bbcf090fcd284aba4c4c3e7b9365e1d
Ashish Bhandari <ashishb0410@gmail.com>
2022-08-01 11:19:47 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const users = {};
 
 io.on("connection", (socket) => {
   socket.on("new-user-joined", (name) => {
-    // console.log("new user", name);
     users[socket.id] = name;
     socket.broadcast.emit("user-joined", name);
   });
